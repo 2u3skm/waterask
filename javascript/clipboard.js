5a18b15cfc2b616f0a37eaf1d38ec43f96bf89ca
Ha1u-8s <64062140+Ha1u-8s@users.noreply.github.com>
2021-02-21 04:05:24 +0900

    Update clipboard.js

--- javascript/clipboard.js
+++ javascript/clipboard.js
@@ -1,8 +1,17 @@
+function ShowLength( str ) {
+  document.getElementById("inputlength").innerHTML = str.length + "文字";
+}
+
 function copy() {
   var text = document.getElementsByTagName("textarea")[0];
   
   text.select();
   document.execCommand("copy");
-  
-  }
-  
+
+}
+
+function clearTextarea() {
+	var textareaForm = document.getElementById("form2");
+  textareaForm.value = '';
+  document.getElementById("inputlength").innerHTML = "0文字";
+}
